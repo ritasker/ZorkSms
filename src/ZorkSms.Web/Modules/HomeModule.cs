1a667dfcbd7b76a8161eafae10f5664abe325091
Richard Tasker <ritasker8t2@gmail.com>
2013-10-26 19:14:43 +0100

    adds index module and page.

---
+++ src/ZorkSms.Web/Modules/HomeModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace ZorkSms.Web.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => { return View["Index"]; };
+        }
+    }
+}
